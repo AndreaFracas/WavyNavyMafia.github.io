91147492e2cffb1f886477b05a784ba3c974d238
AndreaFracas <149299802+AndreaFracas@users.noreply.github.com>
2024-01-08 18:02:25 +0100

    Add files via upload

---
+++ IT-Castoresco.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById('IT-Castoresco').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Set', 'Ott', 'Nov', 'Dic', 'Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago'],
+        datasets: [{
+            label: 'Prezzo',
+            data: [0.01, 0.01, 0.005],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
